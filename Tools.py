1a6914546dc18e6e103f59e510b77ae10e694a88
JianyanLiang <2437811876@qq.com>
2018-12-02 13:34:54 +0800

    add

--- Tools.py
+++ Tools.py
@@ -7,26 +7,34 @@ def get_pixmap(filename, isfile):
         filetype = os.path.splitext(filename)[-1][1:]
         if filetype in ['txt']:
             return QPixmap(':/default/default_filetype/txt.png')
-        elif filetype in ['ppt']:
+        elif filetype in ['ppt', 'pptx']:
             return QPixmap(':/default/default_filetype/ppt.png')
         elif filetype in ['doc', 'docx']:
             return QPixmap(':/default/default_filetype/docx.png')
         elif filetype in ['xls', 'xlsx']:
             return QPixmap(':/default/default_filetype/xlsx.png')
-        elif filetype in ['png', 'jpg', 'jpeg']:
+        elif filetype in ['png', 'jpg', 'jpeg', 'bmp', 'gif', 'jpeg2000', 'tiff']:
             return QPixmap(':/default/default_filetype/jpg.png')
         elif filetype in ['pdf']:
             return QPixmap(':/default/default_filetype/pdf.png')
-        elif filetype in ['mp3']:
+        elif filetype in ['mp3', 'wav','flac']:
             return QPixmap(':/default/default_filetype/mp3.png')
+        elif filetype in ['avi', 'mp4', 'mov', 'rmvb']:
+            return QPixmap(':/default/default_filetype/avi.png')
         elif filetype in ['py']:
             return QPixmap(':/default/default_filetype/py.png')
         elif filetype in ['zip', 'rar', '7z']:
             return QPixmap(':/default/default_filetype/zip.png')
-        elif filetype in ['link']:
-            return QPixmap(':/default/default_filetype/zip.png')
+        elif filetype in ['link', 'html']:
+            return QPixmap(':/default/default_filetype/link.png')
+        elif filetype in ['exe']:
+            return QPixmap(':/default/default_filetype/exe.png')
+        elif filetype in ['apk']:
+            return QPixmap(':/default/default_filetype/apk.png')
+            
         else:
             return QPixmap(':/default/default_filetype/unknown.png')
+        
     else:
         return QPixmap(':/default/default_filetype/folder.png')
 
